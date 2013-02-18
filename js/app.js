fe5a6aa14e8ade4cfff78738e88396f29bd6700e
Rohit Sharma <rohit0981989@gmail.com>
2013-02-18 20:36:29 +0530

    Part 2

--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
 var myApp = angular.module('student', []);
 
-function StudentController($scope){
-  $scope.students = [
-    {name: "Nikhil Jain", age: 27},
-    {name: "Rohit Sharma", age: 23},
-    {name: "Suvish T.", age: 22}
-  ];
-  
+function StudentController($scope, $http){
+  var loadStudents = function(){
+    $scope.students = [];
+
+    $http.get('/students.json').success(
+      function(response, status, headers, config){
+        $scope.students = response.students;
+      }
+    ).error(function(response, status, headers, config){
+      $scope.error_message = response.error_message;
+    });
+  }
+
   $scope.newStudent = function(){
     $scope.enterNew = true;
     $scope.editing = false;
@@ -14,30 +20,52 @@ function StudentController($scope){
   };
   
   $scope.createStudent = function(){
-    $scope.students.push($scope.student);
-    $scope.enterNew = false;
-    $scope.editing = false;
-  };
-  
+    $http.post('/students.json', {"student": $scope.student})
+      .success(function(response, status, headers, config){
+          $scope.students.push(response.student);
+          $scope.enterNew = false;
+          $scope.editing = false;
+        })
+      .error(function(response, status, headers, config){
+        $scope.error_message = response.error_message;
+      });
+  }
+
   $scope.editStudent = function(student){
     $scope.enterNew = false;
     $scope.editing = true;
     $scope.student = student;
   };
   
-  $scope.updateStudent = function(student){
-    $scope.enterNew = false;
-    $scope.editing = false;
+  $scope.updateStudent = function(){
+    $http.put('/students/' + $scope.student.id + '.json', {"student": $scope.student})
+      .success(function(response, status, headers, config){
+          $scope.student = response.student;
+          $scope.enterNew = false;
+          $scope.editing = false;
+        })
+      .error(function(response, status, headers, config){
+        $scope.error_message = response.error_message;
+      });
   };
-  
+
   $scope.cancelSave = function(){
     $scope.enterNew = false;
     $scope.editing = false;
     $scope.student = {};
   };
+
   
   $scope.deleteStudent = function(student){
-    var index = $scope.students.indexOf(student);
-    $scope.students.splice(index,1);
+    $http.delete('/students/' + student.id + '.json')
+      .success(function(response, status, headers, config){
+          var index = $scope.students.indexOf(student);
+          $scope.students.splice(index,1);
+      })
+      .error(function(response, status, headers, config){
+        $scope.error_message = response.error_message;
+      });
   }
+
+  loadStudents();
 }
